97b370d656b4b332449f14992a6fa9e52f0dca13
basileaelterman <basile.aelterman@email.com>
2026-09-09 21:21:11 +0200

    test: add assertion to verify .env file creation in up script test

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from utils import run_script
 
@@ -6,3 +7,4 @@ def test_up_script_creates_env_file() -> bool:
     result: subprocess.CompletedProcess = run_script("scripts/up.sh")
     
     assert result.returncode == 0
+    assert os.path.exists(".env")
